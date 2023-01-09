5f2491446de285545f024fd5db26eab156c262ec
Lime <zong64066406@gmail.com>
2023-01-09 15:14:53 +0800

    update set to the builder

--- src/main/java/com/example/springdemo/service/impl/HsUserServiceImpl.java
+++ src/main/java/com/example/springdemo/service/impl/HsUserServiceImpl.java
@@ -4,14 +4,17 @@ import com.example.springdemo.dao.mapper.HsUserMapper;
 import com.example.springdemo.dao.repository.HsUserRepository;
 import com.example.springdemo.entity.HsUser;
 import com.example.springdemo.tools.SystemTools;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.springdemo.service.model.HsUserService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class HsUserServiceImpl implements HsUserService {
 
     @Autowired
@@ -33,24 +36,29 @@ public class HsUserServiceImpl implements HsUserService {
 
     @Override
     public String save(HsUser hsUser) {
-        if (systemTools.isNullStringTools(hsUser,hsUser.getUsername(),"判斷UserName")){
+        System.out.println(hsUser.getUsername()+"  "+hsUser.getPassword());
+        if (systemTools.isNullStringTools(hsUser,hsUser.getUsername(),"UserName")){
             return "system/login_error";
         }
 
-        if (systemTools.isNullStringTools(hsUser,hsUser.getPassword(),"判斷PassWord")){
+        if (systemTools.isNullStringTools(hsUser,hsUser.getPassword(),"PassWord")){
             return "system/login_error";
         }
+
 
         // 產生 token 如果重複再產生
         String token = systemTools.uuidToken();
         while (hsUserMapper.findToken(token) > 0){
             token = systemTools.uuidToken();
         }
-        hsUser.setToken(token);
+        hsUser.builder().
+                token(token).
+                add_time(LocalDateTime.now()).
+                last_login_time(LocalDateTime.now()).
+                is_admin(0).build();
 
-
-
-        hsUserRepository.save(hsUser);
+        log.info("註冊成功 UserName : { " + hsUser.getUsername()+" }");
+        //hsUserRepository.save(hsUser);
         return "system/login_success";
     }
 
