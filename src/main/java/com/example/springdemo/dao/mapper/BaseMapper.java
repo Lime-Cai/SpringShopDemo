76e2dd148afe4a92f131eff30e3edcaf3f91e160
Lime <zong64066406@gmail.com>
2023-02-20 16:47:20 +0800

    add 2/20

--- src/main/java/com/example/springdemo/dao/mapper/BaseMapper.java
+++
@@ -1,4 +0,0 @@
-package com.example.springdemo.dao.mapper;
-
-public interface BaseMapper<T> {
-}
--- src/main/java/com/example/springdemo/dao/mapper/StoreProductMapper.java
+++ src/main/java/com/example/springdemo/dao/mapper/StoreProductMapper.java
@@ -1,4 +1,10 @@
 package com.example.springdemo.dao.mapper;
+import java.util.List;
+import java.util.Map;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.springdemo.dao.domain.StoreProduct;
 
@@ -9,7 +15,11 @@ import com.example.springdemo.dao.domain.StoreProduct;
 * @Entity com.example.springdemo.dao.domain.StoreProduct
 */
 public interface StoreProductMapper {
-    s
+    List<StoreProduct> selectAllByAdminId(@Param("adminId") Integer adminId);
+
+
+
+
 }
 
 
